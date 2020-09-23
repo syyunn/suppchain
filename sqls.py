2e5a9d170e935abc0dad8967d0f397a6fb31763d
Suyeol Yun <syyun@snu.ac.kr>
2020-09-23 13:05:17 +0900

    add monthly retrieval and to_csv

--- sqls.py
+++ sqls.py
@@ -13,16 +13,25 @@ connection = psycopg2.connect(
 
 conn_status = connection.closed  # 0
 
-try:
-    crsr = connection.cursor()
-    crsr.execute(
-        """select * from raw___uncmtrd.monthly m where period = 202001 and aggregate_level = 2 and trade_flow = 'Imports' and commodity_code ='72' and reporter_code = 842;
-    """
-    )
-    df = pd.DataFrame(crsr.fetchall())
-    print(df)
-except psycopg2.OperationalError:
-    pass
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+years = [str(i) for i in range(2000, 2021)]
+
+for year in years:
+    for month in months:
+        period = int(year + month)
+        print(period)
+        try:
+            crsr = connection.cursor()
+            crsr.execute(
+                f"""select * from raw___uncmtrd.monthly m where period = {period} and aggregate_level = 2 and trade_flow = 'Imports' and commodity_code ='72' and reporter_code = 842;
+            """
+            )
+            df = pd.DataFrame(crsr.fetchall())
+            # print(df)
+            df.to_csv(f"./us_steel_imports_monthly/period{period}_agg2_tfImports_reporterUS_commcode72.csv", sep=",", index=False)
+        except psycopg2.OperationalError:
+            print("error occurred", period)
+            pass
 
 if __name__ == "__main__":
     pass
